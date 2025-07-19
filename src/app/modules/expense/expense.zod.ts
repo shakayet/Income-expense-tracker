462dd56af9405cfa2a100c822518eba3eca6874a
Srabon <srabonshakhawat@gmail.com>
2025-07-19 23:26:51 +0600

    update expense zod

--- src/app/modules/expense/expense.zod.ts
+++ src/app/modules/expense/expense.zod.ts
@@ -1,5 +1,7 @@
 import { z } from 'zod';
 
+// zod for input validation schemas for expense module
+
 export const expenseCreateSchema = z.object({
   amount: z.preprocess(val => Number(val), z.number().positive()),
   category: z.string().min(1),
