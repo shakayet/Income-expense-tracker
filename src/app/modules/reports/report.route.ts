8cca6bf959f226abd5ce0880b9d7c73911cccd4d
Srabon <srabonshakhawat@gmail.com>
2025-07-22 03:17:57 +0600

    Update report route

--- src/app/modules/reports/report.route.ts
+++ src/app/modules/reports/report.route.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { monthlyReport, yearlyReport } from './report.controller';
+import { monthlyReportController, yearlyReportController } from './report.controller';
 import auth from '../../middlewares/auth';
 import { USER_ROLES } from '../../../enums/user';
 
@@ -7,7 +7,7 @@ const router = express.Router();
 
 router.use(auth(USER_ROLES.USER));
 
-router.route('/monthly').get(monthlyReport);
-router.route('/yearly').get(yearlyReport);
+router.route('/monthly').get(monthlyReportController);
+router.route('/yearly').get(yearlyReportController);
 
 export const ReportRoutes = router;
