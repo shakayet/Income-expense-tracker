9c0d85bd5a1cb48336c05917fe697ca8fdb5d8b8
Shakhawat <srabonshakhawat@gmail.com>
2025-08-18 05:22:39 +0600

    Terms & conditions interface added

---
+++ src/app/modules/terms&conditions/interface.ts
@@ -0,0 +1,8 @@
+
+export type ITerms = {
+  content: string; // The T&C text
+  version: string; // Versioning (e.g., v1.0, v2.0)
+  effectiveDate: Date; // When it takes effect
+  createdAt: Date;
+  updatedAt: Date;
+}
