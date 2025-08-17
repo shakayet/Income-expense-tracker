9dd2fc28afc94ce221d904b4797fe16d11cfc8cc
Shakhawat <srabonshakhawat@gmail.com>
2025-08-18 05:29:30 +0600

    Terms & conditions controller added

---
+++ src/app/modules/terms&conditions/controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import { TermsService } from "./service";
+
+const createTerms = async (req: Request, res: Response) => {
+  const result = await TermsService.createTerms(req.body);
+  res.status(201).json({
+    success: true,
+    message: "Terms & Conditions created successfully",
+    data: result,
+  });
+};
+
+const getLatestTerms = async (req: Request, res: Response) => {
+  const result = await TermsService.getLatestTerms();
+  res.status(200).json({
+    success: true,
+    message: "Latest Terms & Conditions retrieved",
+    data: result,
+  });
+};
+
+const getAllTerms = async (req: Request, res: Response) => {
+  const result = await TermsService.getAllTerms();
+  res.status(200).json({
+    success: true,
+    message: "All Terms & Conditions retrieved",
+    data: result,
+  });
+};
+
+export const TermsController = {
+  createTerms,
+  getLatestTerms,
+  getAllTerms,
+};
