331c711efd85964aaac5c0975e724ea1875490b6
Shakhawat <srabonshakhawat@gmail.com>
2025-08-19 03:48:17 +0600

    Update jwtHelper

--- src/helpers/jwtHelper.ts
+++ src/helpers/jwtHelper.ts
@@ -1,11 +1,11 @@
-import jwt, { JwtPayload, Secret } from 'jsonwebtoken';
+import jwt, { JwtPayload, Secret, SignOptions } from 'jsonwebtoken';
 
 const createToken = (payload: object, secret: Secret, expireTime: string) => {
-  return jwt.sign(payload, secret, { expiresIn: expireTime });
+  return jwt.sign(payload, secret, { expiresIn: expireTime } as SignOptions);
 };
 
-const verifyToken = (token: string, secret: Secret) => {
-  return jwt.verify(token, secret) as JwtPayload;
+const verifyToken = (token: string, secret: Secret): JwtPayload | string => {
+  return jwt.verify(token, secret);
 };
 
 export const jwtHelper = { createToken, verifyToken };
