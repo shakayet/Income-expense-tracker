e73f7256b1fd5abe21925b7e3546958d29b4411f
Shakhawat <srabonshakhawat@gmail.com>
2025-08-24 03:00:12 +0600

    Update budget zod

--- src/app/modules/budget/budget.zod.ts
+++ src/app/modules/budget/budget.zod.ts
@@ -5,12 +5,14 @@ export const setBudgetZodSchema = z.object({
     month: z
       .string()
       .regex(/^\d{4}-(0[1-9]|1[0-2])$/, 'Invalid month format (YYYY-MM)'),
+    category: z.string(),
     amount: z.number().positive(),
   }),
 });
 
 export const updateBudgetZodSchema = z.object({
   body: z.object({
+    category: z.string(),
     amount: z.number().positive(),
   }),
 });
