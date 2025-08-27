4fc906cbf19dbac3e613a6a66425667b0821f518
Shakhawat <srabonshakhawat@gmail.com>
2025-08-28 04:09:35 +0600

    Update income interface

--- src/app/modules/income/income.interface.ts
+++ src/app/modules/income/income.interface.ts
@@ -2,7 +2,7 @@ import { Types } from 'mongoose';
 
 export type IIncome = {
   userId: Types.ObjectId;
-  source: 'salary' | 'business' | 'gift' | 'rent' | 'freelancing' | 'others';
+  source: string;
   amount: number;
   date: Date;
   month: string;
