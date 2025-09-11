7c74fed4f8c3b6fb665dc983695300aff616d264
Shakhawat <srabonshakhawat@gmail.com>
2025-09-12 05:44:16 +0600

    Update route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -15,6 +15,25 @@ import { TermsRoutes } from '../app/modules/terms&conditions/routes';
 import { ReviewRoutes } from '../app/modules/review/review.route';
 import { recentTransaction } from '../app/modules/recentTransaction/recentTransaction.route';
 import { stripePayments } from '../stripe/stripeRoute';
+import { InAppPurchaseRoutes } from '../app/modules/inAppPurchase/inAppPurchase.route';
+
+// import express from 'express';
+// import { AuthRoutes } from '../app/modules/auth/auth.route';
+// import { UserRoutes } from '../app/modules/user/user.route';
+// import { ExpenseRoutes } from '../app/modules/expense/expense.route';
+// import { IncomeRoutes } from '../app/modules/income/income.route';
+// import { BudgetRoutes } from '../app/modules/budget/budget.route';
+// import { ReportRoutes } from '../app/modules/reports/report.route';
+// // import path from 'path';
+// // import { NotificationRoutes } from '../app/modules/notification/notification.route';
+// import { PlanRoutes } from '../app/modules/plan/plan.routes';
+// import { SubscriptionRoutes } from '../app/modules/subscription/subscription.routes';
+// import { CategoryRoutes } from '../app/modules/category/category.route';
+// import comparePriceRoutes from '../app/modules/price-compare/compare.route';
+// import { TermsRoutes } from '../app/modules/terms&conditions/routes';
+// import { ReviewRoutes } from '../app/modules/review/review.route';
+// import { recentTransaction } from '../app/modules/recentTransaction/recentTransaction.route';
+// import { stripePayments } from '../stripe/stripeRoute';
 
 const router = express.Router();
 
@@ -22,6 +41,10 @@ const apiRoutes = [
   {
     path: '/user',
     route: UserRoutes,
+  },
+  {
+    path: '/in-app-purchase',
+    route: InAppPurchaseRoutes,
   },
   {
     path: '/auth',
