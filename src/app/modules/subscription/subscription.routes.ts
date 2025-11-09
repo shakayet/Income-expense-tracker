c520ad164b1a02c0f2518f3a13b38e62e5bf68b5
Shakhawat <srabonshakhawat@gmail.com>
2025-11-09 22:44:46 +0600

    Update subscription route

--- src/app/modules/subscription/subscription.routes.ts
+++ src/app/modules/subscription/subscription.routes.ts
@@ -1,19 +1,20 @@
-import express from "express";
-import auth from "../../middlewares/auth";
-import { USER_ROLES } from "../../../enums/user";
-import { SubscriptionController } from "./subscription.controller";
-
+import express from 'express';
+import auth from '../../middlewares/auth';
+import { USER_ROLES } from '../../../enums/user';
+import { SubscriptionController } from './subscription.controller';
 
 const router = express.Router();
 
-router.get("/", 
-    auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN), 
-    SubscriptionController.subscriptions
+router.get(
+  '/',
+  auth(USER_ROLES.SUPER_ADMIN, USER_ROLES.ADMIN),
+  SubscriptionController.subscriptions
 );
 
-router.get("/my-plan", 
-    auth(USER_ROLES.USER), 
-    SubscriptionController.subscriptionDetails
+router.get(
+  '/my-plan',
+  auth(USER_ROLES.USER),
+  SubscriptionController.subscriptionDetails
 );
 
 export const SubscriptionRoutes = router;
